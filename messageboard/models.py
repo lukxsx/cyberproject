4ee87f1c061cebb4433c38904936be473a0c6e7a
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2021-12-25 22:14:33 +0200

    Viewing of posts

--- messageboard/models.py
+++ messageboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post_text = models.CharField(max_length=500)
+    post_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.post_text
--- messageboard/views.py
+++ messageboard/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    post_list = Post.objects.order_by('-post_date')[:5]
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, 'index.html', context)
+
+def postview(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post.html', {'post_text': post})
