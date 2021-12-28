4963d95f0f34725b8be4bcf4cf9e13f5b3eb3bee
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2021-12-28 12:17:05 +0200

    Adding posts to threads work, improved visual layout

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:thread_id>/', views.threadview, name='threadview'),
     path('addthread/', views.addthread, name='addthread'),
+    path('<int:thread_id>/add/', views.addpost, name='addpost'),
+
 ]
--- messageboard/views.py
+++ messageboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Post, Thread
 from django.utils import timezone
 
@@ -12,8 +13,8 @@ def index(request):
 	return render(request, 'index.html', context)
 
 def threadview(request, thread_id):
-    post_list = Post.objects.filter(thread=thread_id).order_by('post_date')
-    return render(request, 'thread.html', {'post_list': post_list})
+    t = get_object_or_404(Thread, pk=thread_id)
+    return render(request, 'thread.html', {'thread': t})
     
 
 def addthread(request):
@@ -27,3 +28,12 @@ def addthread(request):
 		'thread_list': thread_list,
 	}
 	return render(request, 'index.html', context)
+
+def addpost(request, thread_id):
+	t = get_object_or_404(Thread, pk=thread_id)
+	message = request.POST['message']
+	p = Post(post_text=message, post_date=timezone.now(), thread=t)
+	p.save()
+	
+	return HttpResponseRedirect(reverse('threadview', args=(t.id,)))
+
